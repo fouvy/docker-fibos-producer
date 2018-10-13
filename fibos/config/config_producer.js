e8359bc4c8ae847cc496dd11778014691a1889a0
Fouvy <fouvey@gmail.com>
2018-10-13 18:51:37 +0800

    add all files

---
+++ fibos/config/config_producer.js
@@ -0,0 +1,11 @@
+var account = "xxxx-your-account-name"; //fibos account name
+var priKey = "xxxx-your-private-key";
+var pubKey = "xxx-your-public-key";
+module.exports = {
+    "producer-name": account,
+    "producer-pubKey": pubKey,
+    "producer-priKey": priKey,
+    chainId: "6aa7bd33b6b45192465afa3553dedb531acaaff8928cf64b70bd4c5e49b7ec6a",
+    httpEndpoint: "http://ca-rpc.fibos.io:8870",
+    url: "http://xxxx-your-website-url",
+}
